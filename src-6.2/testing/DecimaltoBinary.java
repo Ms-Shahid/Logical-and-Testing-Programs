669bb99e632d7585f0da2600f530914538e8580f
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-31 14:29:00 +0530

    performs decimal to binary

--- src-6.2/testing/DecimaltoBinary.java
+++ src-6.2/testing/DecimaltoBinary.java
@@ -1,22 +1,23 @@
 package testing;
+
 import java.util.Scanner;
 
 public class DecimaltoBinary {
 
 	public static int[] toBinary(int num) {
-		
+
 		String concat = "";
 		while (num != 0) {
-			concat = (num%2) + concat;
+			concat = (num % 2) + concat;
 			num /= 2;
 		}
-		
+
 		while (concat.length() % 4 != 0) {
 			concat = 0 + concat;
 		}
 		return stringToIntArray(concat);
 	}
-	
+
 	public static int[] stringToIntArray(String bin) {
 		int[] binary = new int[bin.length()];
 		for (int i = 0; i < binary.length; i++) {
@@ -24,22 +25,21 @@ public class DecimaltoBinary {
 		}
 		return binary;
 	}
-	
+
 	static void showArr(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]);
 		}
 		System.out.println();
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter a Number");
-		int[] binary =toBinary(s.nextInt());
+		int[] binary = toBinary(s.nextInt());
 		System.out.println("binary representation is: ");
 		showArr(binary);
 		s.close();
 	}
 
-	
 }
