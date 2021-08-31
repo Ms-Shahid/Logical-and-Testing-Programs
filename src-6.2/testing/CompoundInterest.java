b70fb6439beed59cefe532fefd5bd1faa35991c9
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-31 14:22:55 +0530

    Calculate compound interest using formula

--- src-6.2/testing/CompoundInterest.java
+++ src-6.2/testing/CompoundInterest.java
@@ -8,22 +8,24 @@ public class CompoundInterest {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter Principal Amount : ");
 		int P = scan.nextInt();
-		
+
 		System.out.print("\nEnter year : ");
 		int Y = scan.nextInt();
-		
+
 		System.out.print("\nEnter Rate of Interest: ");
 		int R = scan.nextInt();
+
 		scan.close();
-		
-		double payment = monthlyPayment(P,Y,R);
-		System.out.println("The Compound Interest is: "+ payment);
+
+		double payment = monthlyPayment(P, Y, R);
+		System.out.println("The Compound Interest is: " + payment);
 	}
+
 	public static double monthlyPayment(double P, double Y, double R) {
 		int n = (int) (12 * Y);
-		double r = R / (12*100);
-		
-		double payment = (P*r)/(1- Math.pow((1+r),-n));
+		double r = R / (12 * 100);
+
+		double payment = (P * r) / (1 - Math.pow((1 + r), -n));
 		return payment;
 	}
 }
