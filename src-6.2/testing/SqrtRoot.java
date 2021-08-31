1deae1234de348596d743c10de263c2f015f0f17
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-31 14:30:30 +0530

    square root of number using Newton's method

--- src-6.2/testing/SqrtRoot.java
+++ src-6.2/testing/SqrtRoot.java
@@ -1,24 +1,26 @@
 package testing;
+
 import java.util.Scanner;
 
 public class SqrtRoot {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Enter a number to compute Square root: ");
 		double sqrt = scan.nextDouble();
 		System.out.println("Square root is " + calculateSqrt(sqrt));
 		scan.close();
 	}
+
 	public static double calculateSqrt(double c) {
-		
-		double t=c;
+
+		double t = c;
 		double epsilon = 1e-15;
-		while ((Math.abs(t-c/t)) > epsilon * t) {
-			t = (c / t + t)/2;
+		while ((Math.abs(t - c / t)) > epsilon * t) {
+			t = (c / t + t) / 2;
 		}
-		
+
 		return t;
 	}
 }
